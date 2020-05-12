3fef76d432054eaee4c79388e4bdf68ad5078f64
andre-rondon <rondon@estudiorondon.com.br>
2020-05-11 18:41:40 -0700

    bonus almost done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,29 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+  
+    def create
+      user = User.find_by_email params[:email]
+      # has_secure_password gives us the instance method authenticate used to check passwords
+      # user& is a little bit of meta-programming. This basically checks if user is NOT nil before trying to call .authenticate. This is to avoid no method errors.
+      if user&.authenticate params[:password]
+        session[:user_id] = user.id
+        # session is a hash that is available anywhere. Rails will grab all the informtion from session and create a cookie wiht the data.
+        # A cookie is just a string of key value pairs
+        # { session: {user_id: 384, color: 'blue'} }
+        flash[:success] = "User Logged In"
+        redirect_to tweets_path
+      else
+        flash[:warning] = "Couldn't log In"
+        render :new
+      end
+    end
+  
+    def destroy
+      session[:user_id] = nil
+      redirect_to tweets_path
+    end
+
+
 end
